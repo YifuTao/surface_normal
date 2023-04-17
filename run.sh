c423d2bca50ce66626b676fc37aea8f725d88647
YifuTao <ethantao96@gmail.com>
2023-04-17 17:36:24 +0100

    process multiple folders

--- run.sh
+++ run.sh
@@ -17,10 +17,24 @@
 ./build/surface_normal frontier_15 ./asserts/1677223479.258961832_euc.png ./asserts/sn_frontier_15.png 1000.0
 
 # follow the above format to compute normal for all depth images in a folder
-# DEPTH_DIR='/home/yifu/data/nerfstudio/carla/town02_loop/depth_encoding_256'
-# NORMAL_DIR='/home/yifu/data/nerfstudio/carla/town02_loop/normal'
-# mkdir -p $NORMAL_DIR
-# for file in $DEPTH_DIR/*.png; do
-#     echo $file
-#     ./build/surface_normal carla $file $NORMAL_DIR/$(basename $file) 256.0
-# done
+DEPTH_DIRS=(
+    '/home/yifu/data/nerfstudio/frontier/bodleian/euc_depths_256_accum_11/rgbcam_0_front'
+    '/home/yifu/data/nerfstudio/frontier/bodleian/euc_depths_256_accum_11/rgbcam_1_left'
+    '/home/yifu/data/nerfstudio/frontier/bodleian/euc_depths_256_accum_11/rgbcam_2_right'
+)
+NORMAL_DIRS=(
+    '/home/yifu/data/nerfstudio/frontier/bodleian/euc_depths_256_accum_11_normal/rgbcam_0_front/'
+    '/home/yifu/data/nerfstudio/frontier/bodleian/euc_depths_256_accum_11_normal/rgbcam_1_left/'
+    '/home/yifu/data/nerfstudio/frontier/bodleian/euc_depths_256_accum_11_normal/rgbcam_2_right/'
+)
+for ((i=0; i<${#DEPTH_DIRS[@]}; i++)); do
+
+    DEPTH_DIR=${DEPTH_DIRS[$i]}
+    NORMAL_DIR=${NORMAL_DIRS[$i]}
+    mkdir -p $NORMAL_DIR
+    for file in $DEPTH_DIR/*.png; do
+        echo $file
+        ./build/surface_normal frontier_15 $file $NORMAL_DIR/$(basename $file) 256.0
+    done
+done
+
